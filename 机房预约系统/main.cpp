55faca209a9f14e9f58794d3c5957c0177f0b8af
youxuehu <youxuehu@hotmail.com>
2024-04-24 21:07:39 +0800

    Initial Commit

---
+++ 机房预约系统/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  机房预约系统
+//
+//  Created by youxuehu on 2024/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
