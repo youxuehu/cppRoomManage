d26c4437fbedcfac3b3fe4c89aaf8f94216315f7
youxuehu <youxuehu@hotmail.com>
2024-04-25 21:49:40 +0800

    fix

--- 机房预约系统/main.cpp
+++ 机房预约系统/main.cpp
@@ -7,7 +7,60 @@
 #pragma once
 #include <iostream>
 using namespace std;
+#include "string"
+#include "fstream"
 
+
+#include "Identify.hpp"
+
+void LoginIn(string fileName, int type) {
+    // 定义父类指针，用于指向子类实现的对象
+    Identify * person = NULL;
+    
+    // 读文件
+    ifstream ifs;
+    
+    ifs.open(fileName, ios::in);
+    
+    //判断文件是否存在
+    if(!ifs.is_open()) {
+        cout << "文件不存在" << endl;
+        ifs.close();
+        
+        return;
+    }
+    
+    //准备接收用户的信息
+    
+    int id = 0;
+    string name;
+    string pwd;
+    
+    //判断身份
+    if(type == 1) {
+        // 学生身份
+        cout << "请输入你的学号：" << endl;
+        
+        cin >> id;
+        
+    } else if(type == 2) {
+        // 学生身份
+        cout << "请输入你的职工号：" << endl;
+        
+        cin >> id;
+        
+    }
+    
+    cout << "请输入用户名：" << endl;
+    cin >> name;
+    
+    cout << "请输入密码：" << endl;
+    cin >> pwd;
+    
+    
+    
+    
+}
 
 int main(int argc, const char * argv[]) {
     int select = 0;
